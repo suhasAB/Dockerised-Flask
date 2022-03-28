17af33eea50003773a72332161b0b539adc9a6d1
Suhas Anand Balagar <suhasbalagar@gmail.com>
2022-03-28 16:43:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello from Flask app deployed on GCP by Suhas Anand </h1> <br> <h2>CMPE 281 Lab 2- Deploying Flask on GCP</h2>"
+    return "<h1>Hello from DockerizedFlask app  </h1> <br> <h2>deployed on GCP by Suhas Anand</h2>"
 
 
 if __name__ == "__main__":
